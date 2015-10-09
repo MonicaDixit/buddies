3d9e5d020874344b2a6b878dc34a0b5d0860e17b
Monica <mdixit81@gmail.com>
2015-10-08 23:04:47 -0700

    added basic buddy list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,66 @@
  */
 angular.module('buddylistApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+
+    $scope.currentstatus = "";
+    $scope.buddylist = [
+      {
+        fname:"Ross",
+        lname:"Gellar",
+        username:"RossG",
+        status:"Available",
+        image:"images/ross.jpeg"
+      },
+
+      {
+        fname:"Monica",
+        lname:"Bing",
+        username:"MoniB",
+        status:"Available",
+        image:"images/monica.jpeg"
+
+      },
+      { 
+        fname:"Chandler",
+        lname:"Bing",
+        username:"ChandlerB",
+        status:"Offline",
+        image:"images/chandler.jpeg"
+      },
+      {
+       fname:"Pheobe",
+        lname:"Buffey",
+        username:"pheebs",
+        status:"Away",
+        image:"images/pheebs.jpeg"
+
+      },
+      {
+       fname:"Joey",
+        lname:"Trib",
+        username:"JoeyT",
+        status:"Offline",
+        image:"images/joe.jpeg"
+      },
+
+      {
+       fname:"Rachel",
+        lname:"Green",
+        username:"RachG",
+        status:"Away",
+        image:"images/rachel.jpeg"
+      },
+     
     ];
+
+    $scope.setstatus = function(status){
+      $scope.currentstatus = status;
+    };
+
+    $scope.addBuddy = function(){
+
+    };
+
+    
+
   });
