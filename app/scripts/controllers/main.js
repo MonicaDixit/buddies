dccc223a7ccfac14e3645931f9beb5ce3f513591
Monica <mdixit81@gmail.com>
2015-10-10 23:04:14 -0700

    adding firebase

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,69 @@
  * Controller of the buddylistApp
  */
 angular.module('buddylistApp')
-  .controller('MainCtrl', function ($scope, $modal) {
+  .controller('MainCtrl', function ($scope, $modal, $firebaseArray) {
+
+     var ref = new Firebase("https://code123.firebaseio.com/buddylist"); 
+     var buddyref = ref.child("buddies");
     $scope.currentstatus = "";
     $scope.more = false;
+
+    
+
+    // $scope.initial = function(){
+    //   buddyref.set({
+    //   RossG: {
+    //     firstname: 'Ross',
+    //     lastname: 'Gellar',
+    //     username: 'RossG',
+    //     status : 'Available',
+    //     image:"images/ross.jpeg"
+    //   },
+      
+    //   MoniB: {
+    //     firstname: 'Monica',
+    //     lastname: 'Bing',
+    //     username: 'MoniB',
+    //     status : 'Available',
+    //     image:"images/monica.jpeg"
+    //   },
+
+    //   Channy: {
+    //     firstname: 'Chandler',
+    //     lastname: 'Bing',
+    //     username: 'Channy',
+    //     status : 'Away',
+    //     image:"images/chandler.jpeg"
+    //   },
+
+    //   RachG: {
+    //     firstname: 'Rachel',
+    //     lastname: 'Green',
+    //     username: 'RachG',
+    //     status : 'Away',
+    //     image:"images/rachel.jpeg"
+    //   },
+      
+    //   JoeyT: {
+    //     firstname: 'Joey',
+    //     lastname: 'Trib',
+    //     username: 'JoeyT',
+    //     status : 'Offline',
+    //     image:"images/joe.jpeg"
+    //   },
+
+    //   pheebs: {
+
+    //     firstname: 'Pheobe',
+    //     lastname: 'Buffey',
+    //     username: 'pheebs',
+    //     status : 'Offline',
+    //     image:"images/pheebs.jpeg"
+    //   }
+    // });
+
+    // };
+
     $scope.buddylist=[{
           firstname: 'Ross',
           lastname: 'Gellar',
